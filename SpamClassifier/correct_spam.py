762e02614d641b47dd9050c3ae5ccd47872effb9
Sukesh0513 <48705326+Sukesh0513@users.noreply.github.com>
2019-12-13 20:55:38 +0100

    Added comments
    Added comments

--- SpamClassifier/correct_spam.py
+++ SpamClassifier/correct_spam.py
@@ -3,10 +3,12 @@ list_heading = []
 for x in range(0 , 58):
     list_heading.append(str(x))
 myDataframe = pd.read_csv("spambase.data", names = list_heading)
+
+# Taking the sample for training
 k = int(4601*0.8)
-
-
 training_set = myDataframe.sample(k)
+
+# generating the hypothesis list basedon the instances of the Training data
 r, c = training_set.shape
 list_and = [None]*57
 list_or_duplicate = [None]*r
@@ -25,9 +27,11 @@ list_valued = []
 for x in range(0 , 57):
      if len(list_and[x]) < 60:
             list_valued.append(x)
-
+# Taking the sample for the training
 l = int(4601*0.2)
 testing_set = myDataframe[0:4600]
+
+# Teating the hypothesis generated above using the test data set
 r, c = testing_set.shape
 n = 0
 for y in range(0, r):
@@ -40,12 +44,5 @@ for y in range(0, r):
     if count == len(list_valued) and testing_set["57"].iloc[y] == 0:
         n = n + 1
 
-
-
-print(100 - (n/r)*100)
-
-
-
-
-
-
+# printing the 
+print("Accuracy : "+str(100 - (n/r)*100))
